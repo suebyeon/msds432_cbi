5875fc17954991c1f0474475e8864cb5bed418b5
Su Hyun Byeon <53916951+suebyeon@users.noreply.github.com>
2025-03-08 23:21:00 -0600

    Update main.go

--- main.go
+++ main.go
@@ -594,8 +594,8 @@ func GetCovidDetails(db *sql.DB) {
 	create_table := `CREATE TABLE IF NOT EXISTS "covid" (
 		"id" SERIAL,	
 		"zip_code" VARCHAR(255),
-		"week_number INTEGER,
-		"tests"	INTEGER,
+		"week_number" INTEGER,
+		"tests" INTEGER,
 		"percentage_positive" FLOAT,
 		PRIMARY KEY ("id")
 	);`
@@ -684,13 +684,12 @@ func GetCCVIDetails(db *sql.DB) {
 	}
 
 	create_table := `CREATE TABLE IF NOT EXISTS "ccvi" (
-		"community_area_or_zip"   INTEGER,
+		"community_area_or_zip" INTEGER,
 		"geography_type" VARCHAR(255),
 		"community_area_name" VARCHAR(255),
 		"ccvi_category" VARCHAR(255),
 		PRIMARY KEY ("community_area_or_zip")
 	);`
-
 	_, _err := db.Exec(create_table)
 	if _err != nil {
 		panic(_err)
